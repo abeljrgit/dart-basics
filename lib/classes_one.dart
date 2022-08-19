0e229db139f90d7f2f5372e062a0be4fa65cc12a
ABEL\abelm <abelmanuellugtujr1996@gmail.com>
2022-08-19 18:43:35 +0800

    dart-basics

--- lib/classes_one.dart
+++ lib/classes_one.dart
@@ -1,4 +1,8 @@
 class A {
+  A(int someNumber) {
+    this.a = someNumber;
+  }
+
   int? _private;
   int? a;
   final int c = 2;
@@ -18,12 +22,52 @@ class A {
   }
 }
 
-class B extends Object {}
+class B extends Object {
+  B(this.hello, {this.hello2, required this.x, required this.y});
+
+  B.zero()
+      : x = 0,
+        y = 0;
+
+  B.twoX(hello, {required int y})
+      : this(hello, hello2: "Default Hello2", x: 2, y: y);
+  B.twoY(hello, {required int x})
+      : this(hello, hello2: "Defailt Hello2", x: x, y: 2);
+
+  String? hello;
+  String? hello2;
+
+  final int x;
+  final int y;
+}
 
 void someFunctionShowingClassCapabilities() {
-  var a = A();
-  var a2 = A();
-  print(A().toString());
+  var a = A(360);
+  var a2 = A(360);
+  print(a2.toString());
+  print(a._private);
+  a.e = 100;
+  print(a.e);
+  print(a.a);
+  A.j = 5;
+  print(A.j);
+
+  var b = B("Abel", hello2: "Lugtu", x: 5, y: 2);
+  var bZero = B.zero();
+  print(b.hello);
+  print(b.hello2);
+  print(b.x);
+  print(b.y);
+  print(bZero.x);
+  print(bZero.y);
+
+  var bTwo = B.twoX("adwa", y: 23);
+  print(bTwo.x);
+  print(bTwo.y);
+
+  var d = D(x: 742, y: 6172);
+  print(d.x);
+  print(d.y);
 }
 
 class C {
@@ -33,4 +77,17 @@ class C {
     int aawdawd = 23;
     int? _private;
   }
+}
+
+class D {
+  const D({
+    required this.x,
+    required this.y,
+  });
+
+  final int x;
+  final int y;
+
+  @override
+  String toString() => 'Point(x: $x, y:$y)';
 }
